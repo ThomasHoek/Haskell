233c482ca02b4138a87a7b570066ea69badf1c96
Thomas Hoek <https://github.com/ThomasHoek/Structured_programming.git>
2019-10-26 14:25:57 +0200

    Added state

--- Project/main.hs
+++ Project/main.hs
@@ -1,30 +1,77 @@
-type Cell a = [Bool]
-type Grid a = (Cell a,Cell a, Cell a,Cell a, Cell a, Cell a)
+type Cell = [Bool]
+type Grid = (Cell ,Cell , Cell ,Cell , Cell , Cell )
 
-up :: Grid a -> Grid a
+data LastMove = MoveLeft | MoveDown | MoveRight | MoveUp deriving(Show, Eq, Ord)
+
+type Move = (LastMove, Cell)
+
+
+anticlockwise :: LastMove -> LastMove
+anticlockwise a
+        | a == MoveLeft = MoveUp
+        | a == MoveDown = MoveLeft
+        | a == MoveRight = MoveDown
+        | a == MoveUp  = MoveRight
+        | otherwise = MoveUp
+
+clockwise :: LastMove -> LastMove
+clockwise a
+        | a == MoveLeft = MoveDown
+        | a == MoveDown = MoveRight
+        | a == MoveRight = MoveUp
+        | a == MoveUp  = MoveLeft
+        | otherwise = MoveUp
+
+
+gameStatus :: Move -> LastMove
+gameStatus (a,b) 
+        | head b = clockwise a
+        | otherwise = anticlockwise a
+
+move :: Move
+move = (MoveLeft, [True])
+        
+move2 :: Move
+move2 = (MoveLeft, [False])
+        
+        
+ --  add state: state up -> do left change state to left
+          -- state left -> do down change state to down
+         -- state down -> do right change state to right
+         -- state right -> do right change state to up
+
+        --  state turn clockwise if false,false
+        -- state turn anti clockwise if true,true       
+
+
+
+
+up :: Grid -> Grid
 up (a, b, c:cx, d, e, f)
             | not $ head b =  (a ++ [True], [c], cx,d,e,f)
             | otherwise =  (a ++ [False], [c], cx,d,e,f)
 
 
-down :: Grid a -> Grid a
+down :: Grid -> Grid
 down (a,b,c,d,e,f)
             | not $ head b = (init a, [last a], True : c,d,e,f)
             | otherwise = (init a, [last a], False : c,d,e,f)
 
 
-left :: Grid a -> Grid a
+left :: Grid -> Grid
 left (a, b, c, d, e,f:fx)
             | not $ head d =  (a,b,c,d ++ [True], [f], fx)
             | otherwise =  (a,b,c,d ++ [False], [f], fx)
 
-right :: Grid a -> Grid a
+right :: Grid  -> Grid
 right (a,b,c,d,e,f)
             | not $ head e = (a,b,c,init d, [last d], True : f)
             | otherwise = (a,b,c,init d, [last d], False : f)
             
 
-grid :: Grid [Bool]
+            
+
+grid :: Grid
 grid = ([False,False,False,False,False], -- x left
         [False],                         -- x curr
         [False,False,False,False,False], -- x right
@@ -32,6 +79,8 @@ grid = ([False,False,False,False,False], -- x left
         [False,False,False,False,False], -- y left
         [False],                         -- y curr
         [False,False,False,False,False]) -- y right
+
+
 
 -- grid = 
 --    ([False,False ..],                -- x left
@@ -42,13 +91,7 @@ grid = ([False,False,False,False,False], -- x left
 --     [False],                         -- y curr
 --     [False,False..])                 -- y right
 
---  add state: state up -> do left change state to left
-          -- state left -> do down change state to down
-         -- state down -> do right change state to right
-         -- state right -> do right change state to up
 
-        --  state turn clockwise if false,false
-        -- state turn anti clockwise if true,true
 
 -- if false,false -> change both to true and do clockwise
 -- if false,true -> change the false to true and do clockwise
